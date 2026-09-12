7729b1fd041a4a0b579414e65e5b7a1294bb8cff
Laura-1011 <lauraizidro1@gmail.com>
2026-09-12 13:40:59 -0300

    Implementar classe abstrata FormaGeometrica

--- FormasGeometricas/src/com/formasgeometricas/FormaGeometrica.java
+++ FormasGeometricas/src/com/formasgeometricas/FormaGeometrica.java
@@ -1,7 +1,16 @@
 package com.formasgeometricas;
 
-public interface FormaGeometrica {
-    double calcularArea();
+public abstract class FormaGeometrica {
 
-    double calcularPerimetro();
+    // 1. Método genérico estruturado (Sua tarefa)
+    public void desenhar() {
+        // Usa o getSimpleName() para imprimir o nome da classe filha que está sendo desenhada
+        System.out.println("Desenhando a forma: " + this.getClass().getSimpleName());
+    }
+
+    // 2. Método abstrato de área (Sua tarefa)
+    public abstract double calcularArea();
+
+    // 3. Método abstrato de perímetro (Adicionado para não quebrar o código da Pessoa 2 e 3)
+    public abstract double calcularPerimetro();
 }
